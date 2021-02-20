08adbae3063ff6b69d3a3da55d9145b240fc732d
dominggo1999 <arnolddominggo19@gmail.com>
2021-02-20 20:22:26 +0700

    adding placeholder component

--- src/App.js
+++ src/App.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import Placeholder from './components/Placeholder';
 
 const App = () => {
   return (
     <div>
-      Placeholder
+      <Placeholder />
     </div>
   );
 };
