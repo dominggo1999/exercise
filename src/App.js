aa667223c9c9050845da2d9431c9493a55db19fd
dominggo1999 <arnolddominggo19@gmail.com>
2021-02-26 20:49:00 +0700

    adding react-bootstrap

--- src/App.js
+++ src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { Route } from 'react-router-dom';
-import Button from './components/Button/Button';
 import Navbar from './components/Navbar/Navbar';
 
 const App = () => {
